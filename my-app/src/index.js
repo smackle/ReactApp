b63737c632e3048899b154e2413323c714859ce7
Subham Kumar <50901169+smackle@users.noreply.github.com>
2022-04-16 12:35:52 +0530

    update

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,17 +1,43 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom'
+
+//CSS
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function BookList(){
+  return (
+  <section className='booklist'> 
+  <Book /> 
+  <Book />
+  <Book />
+  <Book />
+  <Book />
+  <Book />
+  <Book />
+  </section>
+  
+  ) 
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const Book = () => {
+  return <article className= 'book'>
+  <Image></Image> 
+  <Title />
+  <Author />
+  
+  </article>
+}
+
+const Image = () => <img src="https://m.media-amazon.com/images/I/8144Vic9C5L._AC_UY327_FMwebp_QL65_.jpg" alt="" />
+
+const Title = () => <h1>Girl A </h1>
+const Author = () =>  <h4 style={{color: '#617d98', fontSize: '0.75rem', marginTop: '0.25rem'}}>Abigail Dean</h4>
+
+//trying with arrow function with React.createElement method (could be messy if used many elements)
+// const Greeting = () => {
+//   return React.createElement('h1', {}, 'hello world')
+// };
+
+
+ReactDOM.render(<BookList/>, document.getElementById('root'))
+
