bd6d46dbfbb9f229da5c706b7f6931c5a3703c6c
Subham Kumar <50901169+smackle@users.noreply.github.com>
2022-04-24 11:53:53 +0530

    commit

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -4,34 +4,51 @@ import ReactDOM from 'react-dom'
 //CSS
 import './index.css';
 
+//setup vars
+const firstBook = {
+  img: 'https://m.media-amazon.com/images/I/8144Vic9C5L._AC_UY327_FMwebp_QL65_.jpg',
+  title: 'Girl A',
+  author: 'Abigail Dean'
+}
+
+const secondBook = {
+  img: 'https://m.media-amazon.com/images/I/91+5X0bukSL._AC_UY327_FMwebp_QL65_.jpg',
+  title: 'A Gentleman in Moscow',
+  author: 'Amor Towles'
+}
+
+
 function BookList(){
   return (
   <section className='booklist'> 
-  <Book /> 
-  <Book />
-  <Book />
-  <Book />
-  <Book />
-  <Book />
-  <Book />
+  <Book 
+    img={firstBook.img} 
+    title={firstBook.title} 
+    author={firstBook.author}
+  /> 
+  <Book 
+    img={secondBook.img} 
+    title={secondBook.title} 
+    author={secondBook.author} />  
+ 
   </section>
   
   ) 
 }
 
-const Book = () => {
-  return <article className= 'book'>
-  <Image></Image> 
-  <Title />
-  <Author />
-  
+const Book = ({img, title, author}) => {
+  //const {img, title, author}= props
+
+  return(
+     <article className= 'book'>
+  <img src={img} alt="" />
+  <h1>{title}</h1>
+  <h4>{author}</h4>
+
   </article>
+  )
 }
-
-const Image = () => <img src="https://m.media-amazon.com/images/I/8144Vic9C5L._AC_UY327_FMwebp_QL65_.jpg" alt="" />
-
-const Title = () => <h1>Girl A </h1>
-const Author = () =>  <h4 style={{color: '#617d98', fontSize: '0.75rem', marginTop: '0.25rem'}}>Abigail Dean</h4>
+ 
 
 //trying with arrow function with React.createElement method (could be messy if used many elements)
 // const Greeting = () => {
